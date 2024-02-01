445bf54abd1a6d696836046e1b1afdfc2daf7e38
redbeanmk765 <127715673+redbeanmk765@users.noreply.github.com>
2024-02-02 00:31:37 +0900

    SpringBootStart
    JDk 17 Spring 3.2.2 gradle-kotlin

---
+++ SpringBoot/mysongbook/src/main/java/com/redbean/mysongbook/MysongbookApplication.java
@@ -0,0 +1,13 @@
+package com.redbean.mysongbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysongbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysongbookApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/mysongbook/src/test/java/com/redbean/mysongbook/MysongbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbean.mysongbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysongbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
