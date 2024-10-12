51611484137654de4138f8ebf6ede4edc0dc3ac6
redbeanmk765 <127715673+redbeanmk765@users.noreply.github.com>
2024-10-13 02:36:28 +0900

    register3
    nickname and pwcheck
    errormessage function

--- mysongbook/src/app/register/registerForm.tsx
+++ mysongbook/src/app/register/registerForm.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import { ErrorMessage } from "@hookform/error-message";
 
 export type HookFormTypes = {
+    [key: string]: string;
     ID : string;
     email: string;
     password: string;
@@ -12,18 +13,64 @@ export type HookFormTypes = {
     nickName : string;
     
   }
+
+
   
   export default function HookForm(){
-    const { register, handleSubmit, watch, control, formState: { errors} } = useForm<HookFormTypes>();
+    const { register, handleSubmit, getValues, control, formState: { errors} } = useForm<HookFormTypes>();
+
+    function validate (property: string){
+      let errorMessage = errors[property];
+      return(
+      <div className="h-10 ">
+          {errorMessage ? (
+          <>
+              {errorMessage.type === "required" && (
+                <p className=" text-red-500 ">
+                  {errorMessage.message}
+                </p>
+              )}
+              {errorMessage.type === "pattern" && (
+                <p className=" text-red-500 ">
+                  {errorMessage.message}
+                </p>
+              )}
+              {errorMessage.type === "check" && (
+                <p className=" text-red-500 ">
+                  {errorMessage.message}
+                </p>
+              )}
+          </>
+          ) : <p/>}  
+        </div>)
+    }
 
     const onSubmit = (data: HookFormTypes) => {
         console.log("Form submitted.", data)
     };
+
     return (
       <div>
       <form onSubmit={handleSubmit(onSubmit)}>
+
+      <label htmlFor="ID"> 아이디 </label>
         <p>
           <input
+            id="ID"
+            {...register("ID", {
+              required:{
+                value: true,
+                message: "* 필수 입력 항목입니다."}})}
+            type="text"
+            placeholder="ID" />
+        </p>
+        {validate('ID')}
+
+
+        <label htmlFor="email"> 이메일 </label>
+        <p>
+          <input
+            id="email"
             {...register("email", {
               required:{
                 value: true,
@@ -32,55 +79,61 @@ export type HookFormTypes = {
                 value:/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i, 
                 message: "* 이메일 형식이 아닙니다."}})}
             type="text"
-            placeholder="ID" />
+            placeholder="email" />
         </p>
-        
-        <div className="h-10 ">
-          {errors.email ? (
-          <>
-              {errors.email.type === "required" && (
-                <p className=" text-red-500 ">
-                  {errors.email.message}
-                </p>
-              )}
-              {errors.email.type === "pattern" && (
-                <p className=" text-red-500 ">
-                  {errors.email.message}
-                </p>
-              )}
-            </>
-          ) : <p/>}  
-        </div>
+        {validate('email')}
+       
 
+        <label htmlFor="password"> 비밀번호 </label>
         <p>
           <input
+            id="password"
             {...register("password", {
               required:{
                 value: true,
                 message: "* 필수 입력 항목입니다."},
               pattern:{
-                value:/^(?=.*[a-zA-Z0-9@$!%*?&])$/, 
-                message: "* 비밀번호 형식이 아닙니다. 영어 대소문자, 숫자, 특수기호만이 사용 가능합니다."}})}
+                value:/^[a-zA-Z0-9~@$!%*?&]{1,20}$/,
+                message: "* 비밀번호 형식이 아닙니다. 20자 이하의 영어 대소문자, 숫자, 특수기호만( ~, @ , $ , ! , % , * , ? , & )이 사용 가능합니다."}})}
             type="password"
             placeholder="PW" />
         </p>
-        
-        <div className="h-10 ">
-          {errors.password ? (
-          <>
-              {errors.password.type === "required" && (
-                <p className=" text-red-500 ">
-                  {errors.password.message}
-                </p>
-              )}
-              {errors.password.type === "pattern" && (
-                <p className=" text-red-500 ">
-                  {errors.password.message}
-                </p>
-              )}
-            </>
-          ) : <p/>}  
-        </div>
+        {validate('password')}
+
+        <label htmlFor="password"> 비밀번호 확인 </label>
+        <p>
+          <input
+            id="passwordCheck"
+            {...register("passwordCheck", {
+              required:{
+                value: true,
+                message: "* 필수 입력 항목입니다."},
+              validate: {
+                check: (val) => {
+                  if (getValues("password") !== val) {
+                  return "* 비밀번호가 일치하지 않습니다.";
+                }
+              }}})}
+            type="passwordCheck"
+            placeholder="PW CHECK" />
+        </p>
+        {validate('passwordCheck')}
+
+        <label htmlFor="password"> 닉네임 </label>
+        <p>
+          <input
+            id="nickName"
+            {...register("nickName", {
+              required:{
+                value: true,
+                message: "* 필수 입력 항목입니다."},
+              pattern:{
+                value:/^[가-힣a-zA-Z0-9][가-힣a-zA-Z0-9 ]{0,18}[가-힣a-zA-Z0-9]$/,
+                message: "* 2자이상 20자 이하의 영어 대소문자, 한국어, 숫자만이 사용 가능합니다."}})}
+            type="nickName"
+            placeholder="NICKNAME" />
+        </p>
+        {validate('nickName')}
 
       
         
